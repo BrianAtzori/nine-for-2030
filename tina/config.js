6e8d2cb201e8e4a5d718b25721cbe6ac59736122
BrianAtzori <brianatzori@gmail.com>
2023-11-06 10:38:37 +0100

    Trying Permalink Again

--- tina/config.js
+++ tina/config.js
@@ -24,6 +24,7 @@ export default defineConfig({
         name: "articoli",
         label: "Articoli",
         path: "blog/",
+        permalink:"prova",
         fields: [
           {
             type: "string",
